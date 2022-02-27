a5dfdd297694293f491bb5adc8a337b4914e5c8c
ByrdBassPC <byrdbass@gmail.com>
2022-02-26 18:31:58 -0600

    successfully created skeleton structure of blog with updated Models for Comment

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const User = require('./User');
+const BlogPost = require('./BlogPost');
+const Comment = require('./Comment');
+
+Comment.belongsTo(BlogPost, {
+    foreignKey: 'topic_id',
+    onDelete: 'CASCADE'
+})
+BlogPost.hasMany(Comment, {
+    foreignKey: 'topic_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// User.hasMany(Comment, {
+//     foreignKey: 'comment_id'
+// })
+
+
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+module.exports = { User, BlogPost, Comment };
